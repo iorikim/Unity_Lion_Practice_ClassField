febb69e4df66c397b030a2206ceb186f22211e14
iorikim <63327355+iorikim@users.noreply.github.com>
2020-04-09 14:33:05 +0800

    4/9下午 C#功課

--- New Unity Project/Assets/Enemy.cs
+++ New Unity Project/Assets/Enemy.cs
@@ -1,45 +1,54 @@
 ﻿using UnityEngine;
 
-public class Enemy: MonoBehaviour
+public class Enemy : MonoBehaviour
 {
     /// <summary>
     /// 怪物的血量
     /// </summary>
-    [Header("怪物的血量"), Range(50, 500)]
+    [Header("血量"), Range(50, 500)]
+    [Tooltip("怪物的血量")]
     public int HP = 250;
 
     /// <summary>
     /// 怪物的魔力
     /// </summary>
-    [Header("怪物的魔力"), Range(10, 100)]
+    [Header("魔力"), Range(10, 100)]
+    [Tooltip("怪物的魔力")]
     public int MP = 100;
 
     /// <summary>
     /// 怪物的移動速度
     /// </summary>
-    [Header("怪物的移動速度"), Range(1, 50.5f)]
+    [Header("Monster Speed"), Range(1, 50.5f)]
+    [Tooltip("怪物的移動速度")]
     public float SPEED = 10.5f;
 
     /// <summary>
     /// 怪物的攻擊力
     /// </summary>
-    [Header("怪物的攻擊力"), Range(20, 200)]
+    [Header("Monster Attack"), Range(20, 200)]
+    [Tooltip("怪物的攻擊力")]
     public int ATTACK = 50;
 
     /// <summary>
     /// 怪物的防禦力
     /// </summary>
-    [Header("怪物的防禦力"), Range(0, 100)]
-    public int GUARD ;
+    [Header("Monster Guard"), Range(0, 100)]
+    [Tooltip("怪物的防禦力")]
+    public int GUARD;
 
+    /// <summary>
+    /// 裝備
+    /// </summary>
+    [Header("EQUIPMENT")]
+    [Tooltip("裝備")]
+    public string arms;
 
-
-
-
-
-
-
-
-
-
+    /// <summary>
+    /// 是否有帶鑰匙
+    /// </summary>
+    [Header("其他資料")]
+    [Tooltip("是否帶有鑰匙")]
+    public bool Key = false;
+    
 }
