fec46be815732ff596369fd12bd7e74389a228e0
iorikim <63327355+iorikim@users.noreply.github.com>
2020-04-09 10:31:08 +0800

    c#功課4/9上午

---
+++ New Unity Project/Assets/Enemy.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class Enemy: MonoBehaviour
+{
+    /// <summary>
+    /// 怪物的血量
+    /// </summary>
+    [Header("怪物的血量"), Range(50, 500)]
+    public int HP = 250;
+
+    /// <summary>
+    /// 怪物的魔力
+    /// </summary>
+    [Header("怪物的魔力"), Range(10, 100)]
+    public int MP = 100;
+
+    /// <summary>
+    /// 怪物的移動速度
+    /// </summary>
+    [Header("怪物的移動速度"), Range(1, 50.5f)]
+    public float SPEED = 10.5f;
+
+    /// <summary>
+    /// 怪物的攻擊力
+    /// </summary>
+    [Header("怪物的攻擊力"), Range(20, 200)]
+    public int ATTACK = 50;
+
+    /// <summary>
+    /// 怪物的防禦力
+    /// </summary>
+    [Header("怪物的防禦力"), Range(0, 100)]
+    public int GUARD ;
+
+
+
+
+
+
+
+
+
+
+
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# Unity_Lion_Practice_ClassField
- C# LEARN
